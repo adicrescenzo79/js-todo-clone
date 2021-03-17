22c68e75f8812af19ae1313738e54c4e82c4e2d8
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-03-17 17:04:00 +0100

    qualche css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,19 @@
 $(document).ready(function(){
-  // CON CLICK SU AGGIUNGI
+
+  var lista = $('ul.todos');
+  console.log(lista);
+  console.log(lista.text());
+  if (lista.text() == '') {
+    $('ul.todos').hide();
+    console.log(lista.text());
+  }
+
+
+
   $('i.add').click(function(){
-    var template = $('.todo li').clone();
-    var nuovo = $('#nuovo').val();
-
-    if (nuovo) {
-
-      $('#nuovo').val('');
-
-      template.children('span.title').text(nuovo);
-    }
-
-    // CANCELLAZIONE RIGA
-    var fatto = template.children('i.done');
-    fatto.click(function(){
-      $(this).parent('li').remove();
-    })
-
-    // MODIFICA RIGA
-    var modifica = template.children('i.edit');
-
-    modifica.click(function(){
-      var title = $(this).siblings('span.title');
-      var valore = title.text();
-      title.hide();
-      $(this).siblings('input#mod').val(valore).show();
-    })
-
-    var modificato = template.children('input#mod');
-
-    modificato.keyup(function(e){
-      if (e.which == 13) {
-        var valoreNew = $(this).val();
-        var title = $(this).siblings('span.title');
-        title.text(valoreNew).show();
-        $(this).hide();
-      }
-    })
-
-
-
-
-
-    var todos = $('ul.todos');
-    todos.append(template);
-
-
+    $('.aggiunta').removeClass('inactive');
   })
 
-
-  // CON ENTER SU INPUT
   $('input#nuovo').keyup(function(e){
 
     if (e.which == 13) {
@@ -90,11 +54,15 @@ $(document).ready(function(){
         }
       })
 
-
       var todos = $('ul.todos');
       todos.append(template);
 
+      $('.aggiunta').addClass('inactive');
+
+
     }
+
+
   })
 
 
